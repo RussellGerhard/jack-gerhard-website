e0d5e5add9f5b1230ebab0de11e8520d75331423
RussellGerhard <russellgerhard1@gmail.com>
2022-03-12 19:45:18 -0500

    Build responsive mobile menu icon, format menu, get themed social icons

--- scripts/mobile-nav-curtain-overlay.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add event listener for mobile overlay curtain menu
-    document.getElementsByClassName("mobile-nav-glyph-wrapper").addEventListener("touchend", function(){
-        getElementsByClassName("mobile-nav-overlay").classList.remove("hidden");
-    });
-});
