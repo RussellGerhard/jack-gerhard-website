abf91b3b9e0cc81beb44cfa03cfc022942809bb5
RussellGerhard <russellgerhard1@gmail.com>
2022-03-23 17:57:13 -0400

    Implement multiple carousels on one page, shaders are broken and need to be moved form :hover CSS state to JS mouseover event

--- scripts/carousel.js
+++ scripts/carousel.js
@@ -1,52 +1,53 @@
-const slides = document.getElementsByClassName("carousel-item");
-const nextButton = document.getElementById("carousel-button-next");
-const prevButton = document.getElementById("carousel-button-prev");
-const dots = document.getElementsByClassName("dot");
-let position = 0;
-const numberOfSlides = slides.length;
+const carousels = document.getElementsByClassName("carousel");
+const prevButtonList = document.getElementsByClassName("carousel-prev");
+const nextButtonList = document.getElementsByClassName("carousel-next");
+const numberOfCarousels = carousels.length;
+let positions = new Array(numberOfCarousels).fill(0);
+const numSlides = [4,4];
 
-function hideAllSlides() {
-    for (const slide of slides) {
-        slide.classList.remove("carousel-item-visible");
-        slide.classList.add("carousel-item-hidden");
-    }
-}
+function handleMoveToPrevSlide() {
+    // Get carousel's content items and make the current one hidden
+    carousels[this.i].children[0].children[positions[this.i]].classList.remove("carousel-item-visible");
+    // Get carousel's tabs and style the current as inactive 
+    carousels[this.i].children[1].children[positions[this.i]].classList.remove("active-tab");
 
-const handleMoveToNextSlide = function(e) {
-    hideAllSlides();
-
-    // check if last slide
-    if (position === numberOfSlides - 1) {
-        position = 0;
+    // Check if this was the first slide in the carousel
+    if (positions[this.i] === 0) {
+        positions[this.i] = numSlides[this.i] - 1;
     } else {
-        position++;
+        positions[this.i] = positions[this.i] - 1;
     }
 
-    // make current slide visible
-    slides[position].classList.add("carousel-item-visible");
-
-    // update dots to represent current slide
-    dots[position].classList.add("selected-dot")
-    dots[position].checked = true;
+    // Make current slide visible
+    carousels[this.i].children[0].children[positions[this.i]].classList.add('carousel-item-visible');
+    // Activate tab of current slide
+    carousels[this.i].children[1].children[positions[this.i]].classList.add('active-tab');
 }
 
-const handleMoveToPrevSlide = function(e) {
-    hideAllSlides();
+function handleMoveToNextSlide() {
+    // Get carousel's content items and make the current one hidden
+    carousels[this.i].children[0].children[positions[this.i]].classList.remove("carousel-item-visible");
+    // Get carousel's tabs and style the current as inactive 
+    carousels[this.i].children[1].children[positions[this.i]].classList.remove("active-tab");
 
-    // check if last slide
-    if (position === 0) {
-        position = numberOfSlides - 1;
+    // Check if this was the last slide in the carousel
+    if (positions[this.i] === numSlides[this.i] - 1) {
+        positions[this.i] = 0;
     } else {
-        position--;
+        positions[this.i] = positions[this.i] + 1;
     }
 
-    // make current slide visible
-    slides[position].classList.add("carousel-item-visible");
-
-    // update dots to represent current slide
-    dots[position].classList.add("selected-dot")
-    dots[position].checked = true;
+    // Make current slide visible
+    carousels[this.i].children[0].children[positions[this.i]].classList.add('carousel-item-visible');
+    // Activate tab of current slide
+    carousels[this.i].children[1].children[positions[this.i]].classList.add('active-tab');
 }
 
-nextButton.addEventListener("click", handleMoveToNextSlide);
-prevButton.addEventListener("click", handleMoveToPrevSlide);
+for (let i = 0; i < numberOfCarousels; i++){
+    prevButtonList[i].i = i;
+    nextButtonList[i].i = i;
+    prevButtonList[i].addEventListener("touchstart", handleMoveToPrevSlide);
+    nextButtonList[i].addEventListener("touchstart", handleMoveToNextSlide);
+    prevButtonList[i].addEventListener("click", handleMoveToPrevSlide);
+    nextButtonList[i].addEventListener("click", handleMoveToNextSlide);
+};
