ce8347d46832646f327f8defa04d1f76b1c1140f
RussellGerhard <russellgerhard1@gmail.com>
2022-04-11 14:21:55 -0400

    fix API Gateway route

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,5 +1,5 @@
 const form = document.querySelector('form');
-const formURL = 'https://0l38s5jdgl.execute-api.us-east-1.amazonaws.com/';
+const formURL = 'https://0l38s5jdgl.execute-api.us-east-1.amazonaws.com/contactForm';
 const submitButton = document.getElementById('contact-button');
 
 form.onsubmit = (e) => {
